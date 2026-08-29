1442246e17a8b47c04ea0355d88a37c3624cdb61
Aishwarya G <aishwarya12383@gmail.com>
2026-08-29 21:47:40 +0530

    Time: 17 ms (93.47%), Space: 50.2 MB (41.4%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class BSTIterator {
+    Stack<TreeNode> stack = new Stack<>();
+
+    public BSTIterator(TreeNode root) {
+        pushLeft(root);
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+
+        if (node.right != null) {
+            pushLeft(node.right);
+        }
+
+        return node.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    private void pushLeft(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+}
